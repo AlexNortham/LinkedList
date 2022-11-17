5189fae448af5a5145208e0e37c0fcf0c94f0ee6
alexa <alexanderjnortham@gmail.com>
2022-11-17 15:53:42 +0000

    c file made

--- linkedList.c
+++ linkedList.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
+#include "linkedList.h"
 
-typedef struct nodeData{
-    //node data (to be overwritten)
-    int i;
-}Data;
-
-typedef struct node
-{
-    Data* data;
-    struct node* next;
-}Node;
-
-Node* newNode(Data* data);
-
-Node* addNodeAtHead( Node* head, Node *toadd );
-
-void addNodeAfter( Node* location, Node *toAdd );
-
-Node* deleteNextNode( Node* location );
-
-void freeNode( Node* nodeOut );
-
-void traversal(Node* node);
 
 Node* newNode(Data* data){
     Node* new = (Node*)malloc(sizeof(Node));
