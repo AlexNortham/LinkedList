fb10d4fe2c27bd0852c6672cee37432fdac8ea05
alexa <alexanderjnortham@gmail.com>
2022-11-17 15:15:05 +0000

    Linked List header file

---
+++ linkedList.h
@@ -0,0 +1,60 @@
+#header
+
+typedef struct nodeData{
+    //node data (to be overwritted)
+
+}Data;
+
+typedef struct node
+{
+    Data* data;
+    struct node* nextNode;
+};
+
+Node* newNode(Data* data);
+
+Node* addNodeAtHead( Node* head, Node *toadd );
+
+void addNodeAfter( Node* location, Node *toAdd );
+
+Node* deleteNextNode( Node* location );
+
+void freeNode( Node* nodeOut );
+
+void traversal(Node* node);
+
+Node* newNode(Data* data){
+    Node* new = (Node*)malloc(sizeof(Node));
+    new->data = data;
+    new->next = NULL;
+
+    return new;
+}
+
+void freeNode(Node* nodeOut){
+    free(nodeOut -> data);
+    free(nodeOut);
+}
+
+Node addNodeAtHead(Node* head, Node* toadd){
+    toAdd->next = head;
+    return toadd;
+}
+
+void addNodeAfter(Node* location, Node* toadd){
+    toadd->next = location->next;
+    location->next = toadd;
+}
+
+Node* deleteNextNode(Node* location){
+    Node* removed = location->next;
+    location->next = removed->next; 
+}
+
+void traversal(Node* node){
+    if(node != NULL){
+        //insert operation
+        traversal(node->next);
+    }
+    return;
+}
